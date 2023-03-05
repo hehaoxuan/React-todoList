1687dbad4a1368653072b2d700a7365a507df435
maple <851889398@qq.com>
2023-03-05 22:58:28 +0800

    config: 配置web接口访问地址参数

--- todolist_web/src/config/server.js
+++ todolist_web/src/config/server.js
@@ -1,21 +1,23 @@
-export const server = 'http://localhost:3001'
+export const server = process.env.BASE_URL
+  ? "http://139.155.91.186:3001/"
+  : "http://localhost:3001";
 
 export const todo_all = () => {
-    return `${server}`
-}
+  return `${server}`;
+};
 
 export const todo_detail = (id) => {
-    return `${server}/${id}`
-}
+  return `${server}/${id}`;
+};
 
-export const todo_create = () => `${server}/create`
+export const todo_create = () => `${server}/create`;
 
 export const todo_delete = (id) => {
-    return `${server}/${id}`
-}
+  return `${server}/${id}`;
+};
 
 export const todo_reverse = (id) => {
-    return `${server}/${id}`
-}
+  return `${server}/${id}`;
+};
 
-export const todo_clearCompleted = () => `${server}/clear`
+export const todo_clearCompleted = () => `${server}/clear`;

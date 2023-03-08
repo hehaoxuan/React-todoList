2f376f2eb94203e832b06559d11f330a6879261a
maple <851889398@qq.com>
2023-03-08 13:01:20 +0800

    fix: 修复server访问路径为string时报错

--- todolist_server/controllers/todoController.js
+++ todolist_server/controllers/todoController.js
@@ -1,79 +1,96 @@
-const Todo = require('../models/todo')
+const Todo = require("../models/todo");
 
 //获取所有todo
-const todo_all = (req,res) => {
-    Todo.find().sort({
-        createdAt: -1
-    }).then(result =>{
-        // console.log(result);
-        res.send(result);
+const todo_all = (req, res) => {
+  Todo.find()
+    .sort({
+      createdAt: -1,
     })
-}
+    .then((result) => {
+      // console.log(result);
+      res.send(result);
+    });
+};
 
 //todo细节 传入id 返回todo细节
-const todo_detail = (req,res) =>{
-    const id = req.params.id
-    Todo.findById(id).then(result=>{
-        res.send(result)
-    })
-    // const todo = new Todo(req)
-}
+const todo_detail = (req, res) => {
+  const id = req.params.id;
+  if (typeof id === Number) {
+    Todo.findById(id).then((result) => {
+      res.send(result);
+    });
+  }else{
+    res.send('fail')
+  }
+};
 
 //新增todo
-const todo_create = (req,res) =>{
-    let data
-    for(i in req.body){
-        data = JSON.parse(i)
-    }
-    const todo = new Todo(data)
-    todo.save().then(()=>{
-        res.send('success')
-    }).catch((err)=>{
-        // console.log(err);
-        res.send('error')
+const todo_create = (req, res) => {
+  let data;
+  for (i in req.body) {
+    data = JSON.parse(i);
+  }
+  const todo = new Todo(data);
+  todo
+    .save()
+    .then(() => {
+      res.send("success");
     })
-}
+    .catch((err) => {
+      // console.log(err);
+      res.send("error");
+    });
+};
 
 //删除todo
-const todo_remove = (req,res) =>{
-    const id = req.params.id;
-    Todo.findByIdAndDelete(id).then(result=>{
-        res.send('success deleted')
-    }).catch(err=>{
+const todo_remove = (req, res) => {
+  const id = req.params.id;
+  if (typeof id === Number) {
+    Todo.findByIdAndDelete(id)
+      .then((result) => {
+        res.send("success deleted");
+      })
+      .catch((err) => {
         console.log(err);
-    })
-    // const todo = new Todo(req)
-}
+      });
+  }else{
+    res.send('fail')
+  }
+  // const todo = new Todo(req)
+};
 
 // 更新完成/未完成todo
-const todo_reverse = (req,res) =>{
-    let isDone = null
-    for(i in req.body){
-        isDone = JSON.parse(i)
-    }
-    const id = req.params.id;
-    Todo.findByIdAndUpdate(id,{isDone:isDone}).then(msg =>{
-    //    console.log(msg);
-        res.send('success updated')
-    })
-    // const todo = new Todo(req)
-}
+const todo_reverse = (req, res) => {
+  let isDone = null;
+  for (i in req.body) {
+    isDone = JSON.parse(i);
+  }
+  const id = req.params.id;
+  if(typeof id === Number){
+    Todo.findByIdAndUpdate(id, { isDone: isDone }).then((msg) => {
+        //    console.log(msg);
+        res.send("success updated");
+      });
+  }else{
+    res.send('fail')
+  }
+
+  // const todo = new Todo(req)
+};
 
 //删除完成的todo
-const todo_clearCompleted = (req,res) =>{
-    Todo.deleteMany({isDone:{$eq:"true"}}).then(
-        msg => {
-            console.log(msg);
-        }
-    )
-    res.send('success updated')
-}
+const todo_clearCompleted = (req, res) => {
+  Todo.deleteMany({ isDone: { $eq: "true" } }).then((msg) => {
+    console.log(msg);
+  });
+  res.send("success updated");
+};
 
 module.exports = {
-    todo_all,
-    todo_create,
-    todo_detail,
-    todo_remove,
-    todo_reverse,
-    todo_clearCompleted
-}
+  todo_all,
+  todo_create,
+  todo_detail,
+  todo_remove,
+  todo_reverse,
+  todo_clearCompleted,
+};
--- todolist_server/routes/todoRoute.js
+++ todolist_server/routes/todoRoute.js
@@ -10,6 +10,9 @@ const {
 const express = require('express')
 const router = express.Router();
 
+router.get('/favicon.ico', (req, res) => {
+    res.sendStatus(204);
+});
 router.get('/clear',todo_clearCompleted)
 router.post('/create',todo_create)
 router.get('/:id',todo_detail)
